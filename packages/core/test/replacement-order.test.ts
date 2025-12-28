fb355f6cb2355dea173c0c0a4624a5281f9212de
Ivan <>
2025-12-29 00:54:15 +0100

    changes

--- packages/core/test/replacement-order.test.ts
+++ packages/core/test/replacement-order.test.ts
@@ -16,7 +16,7 @@ describe('replacement ordering', () => {
 		await deploy(app => {
 			const stack = new Stack(app, 'stack')
 			const r1 = new Resource(stack, 'r1', { id: '3' }, { replaceOnChanges: ['id'] })
-			new Resource(stack, 'r2', { id: '2', deps: [r1.id] })
+			new Resource(stack, 'r2', { id: '2', deps: [r1.id] }, { replaceOnChanges: ['deps'] })
 		})
 
 		expect(logs).toStrictEqual(['delete:2', 'delete:1', 'create:3', 'create:2'])
